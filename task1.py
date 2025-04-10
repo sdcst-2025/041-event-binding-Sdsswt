fed2069642f5f148c2ce6c9b1e9621649d69a9b6
Sdsswt <terkeurswillem@gmail.com>
2025-04-10 11:23:40 -0700

    update

--- task1.py
+++ task1.py
@@ -1,24 +1,31 @@
 import tkinter as tk
 import playsound as p
 
-def playsound(event):
-    print(event)
-    p.playsound("animals_dogs_x2_barking_small_001.mp3")
 
+
+def playsound1(event):
+    print(event)
+    p.playsound("laser-gun-81720.mp3")
+def playsound2(event):
+    print(event)
+    p.playsound("vintage-car-horn-153264.mp3")
+def playsound3(event):
+    print(event)
+    p.playsound("hood-irony.mp3")
 
 win = tk.Tk()
 win.attributes('-topmost',True)
-l1 = tk.Label(win,text="This button has an event bound by a command")
-l2 = tk.Label(win,text="This button has an event bound by a bind")
 
-b1 =  tk.Button(win,text="Click to play",command="playsound")
-b2 =  tk.Button(win,text="Click to play")
-b2.bind("<Button>",playsound)
-
-
-l1.pack()
+b1 =  tk.Button(win,text="Lazer gun",command="playsound")
+b1.bind("<Button>",playsound1)
 b1.pack()
-l2.pack()
+
+b2 =  tk.Button(win,text="Car horn")
+b2.bind("<Button>",playsound2)
+b2.pack()
+
+b2 =  tk.Button(win,text="Hood irony")
+b2.bind("<Button>",playsound3)
 b2.pack()
 
 win.mainloop()
